d9c7229a46f5acc8e69ff114d733cf6a0ca4fe6e
Mariya <221297@tkmce.ac.in>
2024-11-07 18:28:29 +0000

    stimulation of Basic Test Signals

--- Experiment-1 Stimulation of Basic Test Signals/test_signals.m
+++ Experiment-1 Stimulation of Basic Test Signals/test_signals.m
@@ -1,67 +1,115 @@
+%% Experiment Number 1 - SIMULATION OF BASIC TEST SIGNALS
+
 clc;
-clear all;
+clear;
 close all;
+
+% Unit Impulse 
+t1=-5:1:5;
+y1=[zeros(1,5),ones(1,1),zeros(1,5)];
 subplot(3,3,1);
-t = -5:1:5;
-y = [zeros(1,5),ones(1,1),zeros(1,5)]; stem(t,y);
-xlabel("Time(s)"); 
-ylabel("Amplitude"); 
-title("Unit Impulse Signal");
+stem(t1,y1);
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Unit Impulse');
+
+% Unit Step
+t2=-5:1:5;
+y2=[zeros(1,5),ones(1,6)];
 subplot(3,3,2);
-t2 = 0:0.01:1;
-f = 5;
-y2 = square(2*pi*f*t2);
-stem(t2,y2);
-hold on; 
-plot(t2,y2);
-xlabel("Time(s)");
-ylabel("Amplitude");
-title("Bipolar Pulse Signal"); 
-legend("Discrete","Continuous");
+stem(t2,y2,'DisplayName','Discrete');
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Unit Step');
+
+%Unit Ramp
+t3=0:1:5;
+y3=t3;
 subplot(3,3,3);
-t3 = 0:0.1:1;
-f = 5;
-y3 = abs(square(2*pi*f*t3));
-stem(t3,y3);
-hold on; 
-plot(t3,y3); 
-xlabel("Time(s)");
-ylabel("Amplitude"); 
-title("Unipolar Pulse Signal"); 
-legend("Discrete","Continuous");
+plot(t3,y3,'DisplayName','Continuous');
+hold on;
+stem(t3,y3,'DisplayName','Discrete');
+hold off;
+xlabel('Time Index');
+ylabel('Amplitude');
+title(' Ramp');
+legend show;
+
+% Sin
+t4=0:0.01:1;
+f1=10;
+y4=sin(2*pi*f1*t4);
 subplot(3,3,4);
-t4 = -5:1:5;
-y4 = t4 .*(t4>=0);
-stem(t4,y4);
-hold on
- plot(t4,y4);
-xlabel("Time(s)");
-ylabel("Amplitude"); 
-title("Unit Ramp Signal");
-legend("Discrete","Continuous");
+plot(t4,y4,'DisplayName','Continuous');
+hold on;
+stem(t4,y4,'DisplayName','Discrete');
+hold off;
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Sin');
+legend show;
+
+%Cos
+t5=0:0.01:1;
+f2=10;
+y5=cos(2*pi*f2*t5);
 subplot(3,3,5);
-t5 = 0:0.025:1;
-f = 10;
-y5 = sawtooth(2*pi*f*t5,0.5); stem(t5,y5);
-hold on; plot(t5,y5); xlabel("Time(s)"); ylabel("Amplitude");
-title("Triangular Signal"); legend("Discrete","Continuous");
-subplot(3,3,6);
+plot(t5,y5,'DisplayName','Continuous');
+hold on;
+stem(t5,y5,'DisplayName','Discrete');
+hold off;
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Cos');
+legend show;
+
+%Bipolar
 t6 = 0:0.001:1;
-f = 10;
-y6 = sin(2*pi*f*t6); stem(t6,y6);
-hold on; plot(t6,y6); xlabel("Time(s)"); ylabel("Amplitude"); title("Sine Wave");
-legend("Discrete","Continuous");
-subplot(3,3,7);
+f3 = 10;
+y6 = square(2*pi*f3*t6);
+subplot(3,3,6);
+plot(t6, y6, '-');
+axis([0 1 -2 2]);
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Bipolar');
+
+
+% Unipolar
 t7 = 0:0.001:1;
-f = 10;
-y7 = cos(2*pi*f*t7); stem(t7,y7);
-hold on; plot(t7,y7); xlabel("Time(s)"); ylabel("Amplitude");
-title("Cosine Wave"); legend("Discrete","Continuous");
+f4 = 10;
+y7 = sqrt(square(2*pi*f4*t7));
+subplot(3,3,7);
+plot(t7, y7, '-');
+axis([0 1 -2 2]);
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Unipolar');
+
+% Exponential Decay
+t9=0:0.01:1;
+y9=exp(-t9);
 subplot(3,3,8);
-t8 = -5:1:5;
-y8 = exp(t8); stem(t8,y8); hold on; plot(t8,y8);
-xlabel("Time(s)"); ylabel("Amplitude"); title("Exponential Signal");
-legend("Discrete","Continuous");
+plot(t9,y9, '-', 'DisplayName','Continous');
+hold on;
+stem(t9,y9,'DisplayName','Discrete');
+hold off;
+axis([0 1 -2 2]);
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Decaying Exponential');
+legend show;
+
+% Triangular
+t8=0:0.25:50;
+f8=5;
 subplot(3,3,9);
-t9 = -5:1:5;!
-y9 = [zeros(1,5),ones(1,6)]; stem(t9,y9); xlabel("Time(s)"); ylabel("Amplitude"); title("Unit Step Signal");
+plot(t8,sin(2*pi*f8*t8), 'DisplayName','Continuous');
+hold on;
+stem(t8,sin(2*pi*f8*t8),'DisplayName','Discrete');
+hold off;
+axis([0 10 -1 1]);
+xlabel('Time Index');
+ylabel('Amplitude');
+title('Triangular');
+legend show;
